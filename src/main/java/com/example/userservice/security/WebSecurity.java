49976a5b1b801d6c57b26f7df9a79ccfe503ebbc
nhw0926@gmail.com <nhw0926@gmail.com>
2023-08-27 01:56:15 +0900

    [할일] #empty 섹션6 User Microservice - AuthenticationFilter 추가

--- src/main/java/com/example/userservice/security/WebSecurity.java
+++ src/main/java/com/example/userservice/security/WebSecurity.java
@@ -5,6 +5,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.servlet.Filter;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -12,8 +14,20 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")    // 인증이 된 상태에서만 추가적인 행동이 가능하므로 제약조건 추가
+                .hasIpAddress("192.168.219.103")    // IP
+                .and()
+                .addFilter(getAuthenticationFilter());
+
 
         http.headers().frameOptions().disable();    // frame로 구별된 h2-console 페이지가 정상적으로 나오게하려면 추가
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
     }
 }
